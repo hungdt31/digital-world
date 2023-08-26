63a04243baf2c788540495dafb06ccd286a27b6c
Tri Hung <koikoidth12@gmail.com>
2023-08-26 15:36:04 +0700

    insert data into database

---
+++ server/controllers/insertData.js
@@ -0,0 +1,42 @@
+const Product = require("../models/product");
+const ProductCategory = require("../models/productCategory")
+const categoryData = require("../../data/cate_brand")
+const data = require('../../data/data2.json')
+const asyncHandler = require("express-async-handler");
+const slugify = require('slugify')
+
+const fn = async(product) => {
+    await Product.create({
+        title: product?.name,
+        slug: slugify(product?.name) + Math.round(Math.random()*100) + '',
+        description: product?.description,
+        brand: product?.brand,
+        price: Math.round(Number(product?.price?.match(/\d/g).join(''))/100),
+        category: product?.category[1],
+        quantity: Math.round(Math.random() * 1000),
+        sold: Math.round(Math.random()*100),
+        color: product?.variants?.find(el => el.label === 'Color')?.variants[0]
+    })
+}
+const fn2 = async(cate) => {
+    await ProductCategory.create({
+        title: cate?.cate,
+        brand: cate?.brand
+    })
+}
+const insertProduct = asyncHandler(async (req, res) => {
+    const promises = []
+    for(let product of data) promises.push(fn(product))
+    await Promise.all(promises)
+    return res.status(200).json('Done');
+});
+const insertCategory = asyncHandler(async (req, res) => {
+    const promises = []
+    for(let cate of categoryData) promises.push(fn2(cate))
+    await Promise.all(promises)
+    return res.status(200).json('Done');
+});
+module.exports = {
+    insertProduct,
+    insertCategory
+}
--- server/routes/index.js
+++ server/routes/index.js
@@ -6,6 +6,7 @@ const brandRouter = require('./brand')
 const blogRouter = require("./blog");
 const couponRouter = require("./coupon")
 const orderRouter = require("./order")
+const insertRouter = require("./insert")
 const { notFound, errHandler } = require("../middlewares/errorHandler");
 
 const initRoutes = (app) => {
@@ -17,6 +18,7 @@ const initRoutes = (app) => {
     app.use("/api/brand", brandRouter);
     app.use("/api/coupon", couponRouter);
     app.use("/api/order", orderRouter);
+    app.use("/api/insert", insertRouter);
     app.use(notFound);
     app.use(errHandler);
 };
